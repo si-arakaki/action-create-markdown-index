e5dd580b8a757635a21bd82c66ebeab3e753eac9
si-arakaki <arakaki@system-integrate.com>
2025-10-02 19:08:35 +0900

    edit

--- main.go
+++ main.go
@@ -4,12 +4,31 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"github.com/go-git/go-git/v6"
 )
 
 func main() {
 	workspace := "/github/workspace"
 
-	err := filepath.Walk(workspace, func(path string, info os.FileInfo, err error) error {
+	repo, err := git.PlainOpen(workspace)
+	if err != nil {
+		panic(err)
+	}
+
+	tree, err := repo.Worktree()
+	if err != nil {
+		panic(err)
+	}
+
+	status, err := tree.Status()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("status: %+v\n", status)
+
+	err = filepath.Walk(workspace, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
